548621a7eaa1d337e2df851711064ef2e7bc595f
Kathir Sudan <52427761+Ex-comunicado@users.noreply.github.com>
2021-07-10 19:13:04 +0530

    Update create_db.py

--- blogsite/create_db.py
+++ blogsite/create_db.py
@@ -1,12 +1,14 @@
+#this file will only be used once to create the users database
+#you can delete this after running it once
 import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
-    user = "root",
-    passwd = "password"
+    user = "" #enter your mysql username,
+    passwd = "" #enter your mysql password
 )
 my_cursor = mydb.cursor()
 
-#my_cursor.execute("CREATE DATABASE users")
+my_cursor.execute("CREATE DATABASE users")
 
 my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
